0889477de08248c087e610d0ab2cf75ea5f6e013
Aniketrana <ranaannu93@gmail.com>
2020-07-29 09:59:36 +0530

    Add project files.

---
+++ BundlingAndMinificationInMVC/App_Start/BundleConfig.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Optimization;
+
+
+namespace BundlingAndMinificationInMVC.App_Start
+{
+    public class BundleConfig
+    {
+        public static void RegisterBundle(BundleCollection bundles)
+        {
+            //var bundle = new ScriptBundle("~/Content/js");
+
+            //bundle.Include("~/content/JS/Javascript1.js",
+            //    "~/content/JS/Javascript2.js",
+            //    "~/content/JS/Javascript3.js",
+            //    "~/content/JS/Javascript4.js",
+            //    "~/content/JS/Javascript5.js");
+
+            //bundles.Add(bundle);
+
+            bundles.Add(new ScriptBundle("~/bundles/JS")
+                .IncludeDirectory("~/Content/JS", "*.js"));
+
+            bundles.Add(new ScriptBundle("~/bundles/CSS")
+                .IncludeDirectory("~/Content/Style", "*.css"));
+
+            BundleTable.EnableOptimizations = true;
+        }
+    }
+}
---
+++ BundlingAndMinificationInMVC/Content/JS/JavaScript1.js
@@ -0,0 +1,8 @@
+﻿$(document).ready(function () {
+    //console.log("Hello from JS 1");
+
+    for (var i = 0; i < 5; i++) {
+        var thisVar = i;
+        console.log("Hello from JS 1 i = " + thisVar);
+    }
+})
